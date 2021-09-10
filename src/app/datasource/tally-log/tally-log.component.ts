8d6f592598640176ce43f6728dffe51268f9bb13
Your Name <you@example.com>
2021-09-10 19:08:42 +0800

    dynamic column

--- src/app/datasource/tally-log/tally-log.component.ts
+++ src/app/datasource/tally-log/tally-log.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { TallyLogDataSource, TallyLogItem } from './tally-log-datasource';
 
 @Component({
@@ -13,14 +13,33 @@ export class TallyLogComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<TallyLogItem>;
-  dataSource: TallyLogDataSource;
+  dataSource: any;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'complaints'];
-
+  displayedColumns = [];
+  tableData = [];
+  _maxDateOfTheMonth = 0;
   constructor() {
-    this.dataSource = new TallyLogDataSource();
+    this._maxDateOfTheMonth = this.lastDateOfTheMonth(9, 2021)
 
+    console.log(this._maxDateOfTheMonth)
+    this.columnGenerator()
+    this.dataSource = new MatTableDataSource();
+
+  }
+  private lastDateOfTheMonth(month: number, year: number): number {
+    return new Date(year, month+1, 0).getDate();
+  }
+  private fakeDataGenerator(): void{
+
+  }
+  private columnGenerator(): void{
+    this.displayedColumns.push(`Complaints`)
+    for(let i = 1; this._maxDateOfTheMonth >= i   ; i++){
+      this.displayedColumns.push(`${i}`)
+    }
+    this.displayedColumns.push(`Total`)
+    console.log(this.displayedColumns)
   }
 
   ngAfterViewInit(): void {
