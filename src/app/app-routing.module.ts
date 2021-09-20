80503dcbbe8b6029acd1ffe176552b8f862f8cca
Your Name <you@example.com>
2021-09-20 23:37:13 +0800

    Admin-account ts update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { HeadAdminComponent } from './head-admin/head-admin.component';
-import { StudentAccountComponent} from './student-account/student-account.component';
 import { AdminAccountComponent } from './admin-account/admin-account.component';
+import { StudentAccountComponent} from './student-account/student-account.component';
+import { StaffAccountComponent } from './staff-account/staff-account.component';
+import { FacultyAccountComponent } from './faculty-account/faculty-account.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'head-admin', component: HeadAdminComponent },
-  { path: 'student-account', component: StudentAccountComponent },
   { path: 'admin-account', component: AdminAccountComponent },
+  { path: 'student-account', component: StudentAccountComponent },
+  { path: 'faculty-account', component: FacultyAccountComponent },
+  { path: 'staff-account', component: StaffAccountComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
