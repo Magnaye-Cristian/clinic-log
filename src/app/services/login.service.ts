60261d3907a6051bf8112b6288f56c6ba74dda38
magnayecristianancheta@gmail.com <magnayecristianancheta@gmail.com>
2021-10-19 18:21:06 +0800

    removed unused codes

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -11,15 +11,4 @@ export class LoginService {
 
   constructor(private http: HttpClientHelperService) { }
 
-  login(login: ILogin) {
-    const response = this.http.post('authenticate', login);
-    response.subscribe((result: HttpResponse<Object>) => {
-      if (result.status !== 200)
-        return;
-      const token = result.headers.get('x-auth-token')
-      console.log(`token + ${token}`)
-      localStorage.setItem('token', result.headers.get('x-auth-token'))
-    })
-    return response;
-  }
 }
