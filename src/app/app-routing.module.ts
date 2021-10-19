91cb24c8e1c06c03c42731708d684a4439f021ab
magnayecristianancheta@gmail.com <magnayecristianancheta@gmail.com>
2021-10-19 17:06:04 +0800

    route guard for no token

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeadAdminComponent } from './head-admin/head-admin.component';
 import { AdminAccountComponent } from './admin-account/admin-account.component';
-import { StudentAccountComponent} from './student-account/student-account.component';
+import { StudentAccountComponent } from './student-account/student-account.component';
 import { StaffAccountComponent } from './staff-account/staff-account.component';
 import { FacultyAccountComponent } from './faculty-account/faculty-account.component';
+import { AuthGuard } from './services/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'head-admin', component: HeadAdminComponent },
+  { path: 'head-admin', component: HeadAdminComponent, canActivate: [AuthGuard] },
   { path: 'admin-account', component: AdminAccountComponent },
   { path: 'student-account', component: StudentAccountComponent },
   { path: 'faculty-account', component: FacultyAccountComponent },
