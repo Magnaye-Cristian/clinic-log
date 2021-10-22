293bb4325e47ab454f4781ce39674082d5b3b255
magnayecristianancheta@gmail.com <magnayecristianancheta@gmail.com>
2021-10-22 17:26:21 +0800

    logbook

--- src/app/datasource/logbook-logs/logbook-logs.component.ts
+++ src/app/datasource/logbook-logs/logbook-logs.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
@@ -8,44 +8,57 @@ import { CreateLogComponent } from 'src/app/dialog/create-log/create-log.compone
 import { TerminateComponent } from 'src/app/dialog/terminate/terminate.component';
 import { EditLogComponent } from 'src/app/dialog/edit-log/edit-log.component';
 import { AddMedicineComponent } from 'src/app/dialog/add-medicine/add-medicine.component';
+import { LogService } from 'src/app/services/log.service';
 
 @Component({
   selector: 'app-logbook-logs',
   templateUrl: './logbook-logs.component.html',
   styleUrls: ['./logbook-logs.component.css']
 })
-export class LogbookLogsComponent implements AfterViewInit {
+export class LogbookLogsComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<LogbookLogsItem>;
-  dataSource: LogbookLogsDataSource;
-
+  // dataSource: LogbookLogsDataSource;
+  dataSource: any;
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['srCode', 'name', 'department', 'purpose', 'medicine', 'edit', 'terminate'];
+  displayedColumns = ['school id', 'name', 'department', 'purpose', 'medicine', 'edit', 'terminate'];
 
-  constructor( private dialog: MatDialog ) {
-    this.dataSource = new LogbookLogsDataSource();
+  constructor(private dialog: MatDialog, private logService: LogService) {
+    // this.dataSource = new LogbookLogsDataSource();
+
   }
 
-  onCreate(){
+  ngOnInit(): void {
+    // this.dataSource = [{ school_id: '22-22222', name: 'Juan Dela Cruz', department: 'CICS', purpose: 'medical' }]
+
+    console.log(this.dataSource)
+    this.logService.getLogsNoTimeOut().subscribe(x => {
+      this.dataSource = x;
+
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      this.table.dataSource = this.dataSource;
+      console.log(x)
+    })
+  }
+
+  onCreate() {
     this.dialog.open(CreateLogComponent);
   }
 
-  onEdit(){
+  onEdit() {
     this.dialog.open(EditLogComponent);
   }
 
-  onMedicine(){
+  onMedicine() {
     this.dialog.open(AddMedicineComponent);
   }
 
-  onTerminate(){
+  onTerminate() {
     this.dialog.open(TerminateComponent);
   }
 
   ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
   }
 }
